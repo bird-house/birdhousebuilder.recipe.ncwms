a34d7fe14b9ac417318bc57dfc9e0e9568b80541
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-22 19:16:50 +0200

    using new tomcat recipe with deployment

--- birdhousebuilder/recipe/ncwms/__init__.py
+++ birdhousebuilder/recipe/ncwms/__init__.py
@@ -13,17 +13,13 @@ from mako.template import Template
 
 import zc.buildout
 import zc.recipe.deployment
-from zc.recipe.deployment import make_dir
+from zc.recipe.deployment import Configuration
 import birdhousebuilder.recipe.conda
-from birdhousebuilder.recipe import tomcat
+import birdhousebuilder.recipe.tomcat
+from birdhousebuilder.recipe.tomcat import make_dirs, unzip
 
-config = Template(filename=os.path.join(os.path.dirname(__file__), "config.properties"))
+config_props = Template(filename=os.path.join(os.path.dirname(__file__), "config.properties"))
 wms_config = Template(filename=os.path.join(os.path.dirname(__file__), "config.xml"))
-
-def make_dirs(name, user, mode=0o755):
-    etc_uid, etc_gid = pwd.getpwnam(user)[2:4]
-    created = []
-    make_dir(name, etc_uid, etc_gid, mode, created)
 
 class Recipe(object):
     """This recipe is used by zc.buildout.
@@ -38,40 +34,15 @@ class Recipe(object):
 
         self.logger = logging.getLogger(name)
 
-        # deployment layout
-        def add_section(section_name, options):
-            if section_name in buildout._raw:
-                raise KeyError("already in buildout", section_name)
-            buildout._raw[section_name] = options
-            buildout[section_name] # cause it to be added to the working parts
-
-        self.deployment_name = self.name + "-ncwms-deployment"
-        self.deployment = zc.recipe.deployment.Install(buildout, self.deployment_name, {
-            'name': "ncwms",
-            'prefix': self.options['prefix'],
-            'user': self.options['user'],
-            'etc-user': self.options['etc-user']})
-        add_section(self.deployment_name, self.deployment.options)
-
-        self.options['etc-prefix'] = self.options['etc_prefix'] = self.deployment.options['etc-prefix']
-        self.options['var-prefix'] = self.options['var_prefix'] = self.deployment.options['var-prefix']
-        self.prefix = self.options['prefix']
-
-        # conda environment path
-        self.options['env'] = self.options.get('env', '')
-        self.options['pkgs'] = self.options.get('pkgs', 'ncwms2')
-        self.options['channels'] = self.options.get('channels', 'defaults birdhouse')
-        self.conda = birdhousebuilder.recipe.conda.Recipe(self.buildout, self.name, {
-            'env': self.options['env'],
-            'pkgs': self.options['pkgs'],
-            'channels': self.options['channels'] })
-        self.options['conda-prefix'] = self.options['conda_prefix'] = self.conda.options['prefix']
-
-        # config options
+        # tomcat
+        self.options['pkgs'] = self.options.get('pkgs', 'apache-tomcat ncwms2')
+        self.tomcat = birdhousebuilder.recipe.tomcat.Recipe(self.buildout, self.name, self.options)
+            
+        # ncwms config options
         #self.options['config_dir'] = self.options.get(
         #    'config_dir', os.path.join(tomcat.content_root(self.prefix), 'ncWMS2'))
         self.options['data_dir'] = self.options.get(
-            'data_dir', os.path.join(self.prefix, 'var', 'lib', 'pywps', 'outputs'))
+            'data_dir', os.path.join(self.tomcat.prefix, 'var', 'lib', 'pywps', 'outputs'))
         self.options['contact'] = self.options.get('contact', 'Birdhouse Admin')
         self.options['email'] = self.options.get('email', '')
         self.options['organization'] = self.options.get('organization', 'Birdhouse')
@@ -85,43 +56,34 @@ class Recipe(object):
 
     def install(self, update=False):
         installed = []
-        if not update:
-            installed += list(self.deployment.install())
-        installed += list(self.conda.install(update))
-        installed += list(self.install_tomcat(update))
-        #installed += list(self.install_config())
-        #installed += list(self.install_wms_config())
+        installed += list(self.tomcat.install(update))
+        installed += list(self.install_config())
+        installed += list(self.install_wms_config())
         return installed
 
-    def install_tomcat(self, update):
-        script = tomcat.Recipe(
-            self.buildout,
-            self.name,
-            self.options)
-        return script.install(update)
-
     def install_config(self):
-        result = config.render(**self.options)
+        text = config_props.render(**self.options)
 
         # make sure ncWMS2.war is unpacked
-        #tomcat.unzip(self.env_path, 'ncWMS2.war')
+        unzip(
+            os.path.join(self.tomcat.options['catalina-base']),
+            os.path.join(self.tomcat.options['catalina-home'], 'webapps', 'ncWMS2.war'))
 
-        output = os.path.join(tomcat.tomcat_home(self.env_path), 'webapps', 'ncWMS2', 'WEB-INF', 'classes', 'config.properties')
-        make_dirs(os.path.dirname(output))
-
-        with open(output, 'wt') as fp:
-            fp.write(result)
-        return [output]
+        config = Configuration(self.buildout, 'config.properties', {
+            'deployment': self.tomcat.deployment_name,
+            'directory': os.path.join(self.options['catalina-base'], 'webapps', 'ncWMS2', 'WEB-INF', 'classes' ),
+            'text': text})
+        return [config.install()]
 
     def install_wms_config(self):
-        result = wms_config.render(**self.options)
-
-        output = os.path.join(tomcat.content_root(self.prefix), 'ncWMS2', 'config.xml')
-        make_dirs(os.path.dirname(output))
-
-        with open(output, 'wt') as fp:
-            fp.write(result)
-        return [output]
+        text = wms_config.render(**self.options)
+        #output = os.path.join(tomcat.content_root(self.prefix), 'ncWMS2', 'config.xml')
+        make_dirs(os.path.join(self.options['catalina-base'], 'conf', 'ncWMS2' ), self.options['etc-user'], mode=0o755)
+        config = Configuration(self.buildout, 'config.xml', {
+            'deployment': self.tomcat.deployment_name,
+            'directory': os.path.join(self.options['catalina-base'], 'conf', 'ncWMS2' ),
+            'text': text})
+        return [config.install()]
 
     def update(self):
         return self.install(update=True)
