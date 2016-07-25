2aa82aeed73c8e2ccf51bdf1a98062cafed68ad8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-25 14:27:42 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ This module contains the tool of birdhousebuilder.recipe.ncwms
 from setuptools import find_packages
 from setuptools import setup
 
+name = 'birdhousebuilder.recipe.ncwms'
+
 version = '0.3.0'
 description = 'A Buildout recipe to install and configure ncWMS2 server with Anaconda.'
 long_description = (
@@ -13,18 +15,23 @@ long_description = (
     open('CHANGES.rst').read()
 )
 
-entry_point = 'birdhousebuilder.recipe.ncwms'
-entry_points = {"zc.buildout": [
-                            "default = %s:Recipe" % entry_point,
-                          ],
-                "zc.buildout.uninstall": [
-                            "default = %s:uninstall" % entry_point,
-                          ],
-                       }
+entry_points = '''
+[zc.buildout]
+default = %(name)s:Recipe
+[zc.buildout.uninstall]
+default = %(name)s:uninstall
+''' % globals()
 
-tests_require = ['zope.testing', 'zc.buildout', 'manuel']
+reqs = ['setuptools',
+        'zc.buildout',
+        'zc.recipe.deployment >=1.3.0',
+        'Mako',
+        'birdhousebuilder.recipe.conda >=0.3.1',
+        'birdhousebuilder.recipe.tomcat >=0.3.0',]
+tests_reqs = ['zc.buildout', 'zope.testing']
 
-setup(name='birdhousebuilder.recipe.ncwms',
+
+setup(name=name,
       version=version,
       description=description,
       long_description=long_description,
@@ -38,22 +45,14 @@ setup(name='birdhousebuilder.recipe.ncwms',
       ],
       keywords='buildout recipe wms ncwms tomcat birdhouse conda anaconda',
       author='Birdhouse',
-      author_email='',
+      author_email='wps-dev at dkrz.de',
       url='https://github.com/bird-house/birdhousebuilder.recipe.ncwms',
-      license='BSD',
+      license='Apache License 2',
+      install_requires = reqs,
+      extras_require = dict(tests=tests_reqs),
+      entry_points=entry_points,
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['birdhousebuilder', 'birdhousebuilder.recipe'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
-                        'zc.buildout',
-                        # -*- Extra requirements: -*-
-                        'Mako',
-                        'birdhousebuilder.recipe.conda',
-                        'birdhousebuilder.recipe.tomcat',
-                        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='birdhousebuilder.recipe.ncwms.tests.test_docs.test_suite',
-      entry_points=entry_points,
       )
